a58f7b3dd7db8f8e94b19766302436d722ad3b0e
joe123grant <joe123grant@gmail.com>
2026-03-14 22:06:19 +0000

    Create __init__.py

---
+++ option/__init__.py
@@ -0,0 +1,15 @@
+from .option import Option, Some
+from .path import AsFile, AsDirectory, AsVisibleFile, IsFile, IsDirectory, IsVisible, Exists
+
+__all__ = [
+    "Option", 
+    "Some",
+    "AsFile", 
+    "AsDirectory", 
+    "AsVisibleFile",
+    "IsFile", 
+    "IsDirectory", 
+    "IsVisible", 
+    "Exists",
+]
+
