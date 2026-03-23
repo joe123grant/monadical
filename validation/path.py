ce861d9b1b81d8803ddcf1b5bf575e4f4f1fc66f
joe123grant <joe123grant@gmail.com>
2026-03-23 12:08:07 +0000

    Update path.py

--- validation/path.py
+++ validation/path.py
@@ -5,14 +5,11 @@ from pathlib import Path
 from .validation import Validation
 from ..option.path import AsFile, AsDirectory, AsVisibleFile
 
-
 def ValidateFile(path: str | Path | None) -> Validation[Path, str]:
     return AsFile(path).Match(Validation.Success, lambda: Validation.Fail([f"Not a valid file: {path!r}"]))
 
-
 def ValidateDirectory(path: str | Path | None) -> Validation[Path, str]:
     return AsDirectory(path).Match(Validation.Success, lambda: Validation.Fail([f"Not a valid directory: {path!r}"]))
-
 
 def ValidateVisibleFile(path: str | Path | None) -> Validation[Path, str]:
     return AsVisibleFile(path).Match(Validation.Success, lambda: Validation.Fail([f"Not a valid or visible file: {path!r}"]))
