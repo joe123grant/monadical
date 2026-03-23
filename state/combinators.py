4eeb048f9176378dd227c17ca2bda2d4e278def6
joe123grant <joe123grant@gmail.com>
2026-03-23 12:14:56 +0000

    Update combinators.py

--- state/combinators.py
+++ state/combinators.py
@@ -4,21 +4,18 @@ from collections.abc import Callable, Iterable
 
 from .state import State
 
-
 def Sequence[S, A](states: Iterable[State[S, A]]) -> State[S, list[A]]:
-    def _run(s: S) -> tuple[list[A], S]:
+    def _Run(initialState: S) -> tuple[list[A], S]:
         values: list[A] = []
-        current = s
+        current = initialState
         for state in states:
-            a, current = state.Run(current)
-            values.append(a)
+            value, current = state.Run(current)
+            values.append(value)
         return values, current
-    return State(_run)
-
+    return State(_Run)
 
 def Traverse[S, A, B](items: Iterable[A], func: Callable[[A], State[S, B]]) -> State[S, list[B]]:
     return Sequence(func(item) for item in items)
-
 
 def Replicate[S, A](times: int, state: State[S, A]) -> State[S, list[A]]:
     return state.Replicate(times)
